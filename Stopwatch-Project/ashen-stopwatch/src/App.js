237b716468c7d50631da2764d0df660acab273e5
EddieRAM <edusacarneiro@gmail.com>
2021-07-31 17:29:32 -0300

    project concluded

--- Stopwatch-Project/ashen-stopwatch/src/App.js
+++ Stopwatch-Project/ashen-stopwatch/src/App.js
@@ -1,7 +1,47 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const App = () => {
+  const [timerDays, setTimerDays] = useState('00');
+  const [timerHours, setTimerHours] = useState('00');
+  const [timerMinutes, setTimerMinutes] = useState('00');
+  const [timerSeconds, setTimerSeconds] = useState('00');
+
+  let interval = useRef();
+
+  const startTimer = () => {
+    const countdownDate = new Date ('Oct 21, 2021 00:00:00').getTime();
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countdownDate - now;
+
+      const days =  Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours =  Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
+      const minutes =  Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds =  Math.floor((distance % (1000 * 60) / 1000));
+
+      if (distance < 0) {
+        alert('O intervalo acabou!')
+        clearInterval(interval.current);
+      } else {
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+
+    }, 1000)
+  }
+
+  // componentDidMout
+  useEffect(() => {
+    startTimer();
+    return () => {
+      clearInterval(interval.current);
+    }
+  });
+
   return (
     <section className="timer-container">
       <section className="timer">
@@ -12,21 +52,21 @@ const App = () => {
         </div>
         <div>
           <section>
-            <p>87</p>
+            <p>{ timerDays }</p>
             <p><snall>Days</snall></p>
           </section>
           <span>:</span>
           <section>
-            <p>87</p>
+            <p>{ timerHours }</p>
             <p><snall>Hours</snall></p>
           </section>
           <span>:</span>
           <section>
-            <p>87</p>
+            <p>{ timerMinutes }</p>
             <p><snall>Minutes</snall></p>
           </section>
           <span>:</span><section>
-            <p>87</p>
+            <p>{ timerSeconds }</p>
             <p><snall>Seconds</snall></p>
           </section>          
         </div>
